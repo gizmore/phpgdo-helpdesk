43eafe9fc861887e50e7db6ec772acaadb3da3ae
gizmore <gizmore@wechall.net>
2026-09-18 04:05:07 +0200

    'more korean'

---
+++ lang/helpdesk_ko.php
@@ -0,0 +1,23 @@
+<?php
+return [
+	'cfg_helpdesk_attachments' => '첨부파일을 허용하시겠습니까?',
+	'cfg_hook_right_bar' => '오른쪽 메뉴 표시줄에 연결하시겠습니까?',
+	'link_helpdesk' => '헬프 데스크',
+	'mt_helpdesk_openticket' => '티켓 생성',
+	'mail_subj_helpdesk_ticket_created' => '[%s] 새 헬프데스크 티켓',
+	'mail_body_helpdesk_ticket_created' => '안녕하세요 %s님,
+
+새로운 헬프데스크 티켓이 생성되었습니다.
+
+------------
+보낸 사람: %s
+제목: %s
+메시지: %s
+------------
+
+이 티켓을 청구하여 작업할 수 있습니다.
+%s
+
+친절한 감사
+%s 봇',
+];
